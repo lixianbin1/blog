8b09541245d0d2e0e676dbe8cabcafe6692ea0f0
李贤斌 <lixianbin@formssi.com>
2023-02-06 16:17:27 +0800

    Site updated: 2023-02-06 16:17:24

---
+++ js/tools.js
@@ -0,0 +1,31 @@
+(()=>{
+
+  //设置 set-li 的 marginn负数；通过css媒体查询设置屏幕低于769的都为 transform锁放
+  function resize(){
+    if(window.innerWidth<=769){
+      $('.set-li').map(function(e,a){
+        var height=$(a).height()/2
+        $(a).css('margin-bottom',-height)
+      })
+    }else{
+      $('.set-li').css('margin-bottom','auto')
+    }
+    // 设置table样式
+    $('.passage-article>table').map(function(index,a){
+      let fwidth=$('.passage-article').width()
+      let cwidth=$(a).width()+2
+      let cheight=$(a).height()+2
+      let ratio=fwidth/cwidth
+      let minus=cheight-(cheight*fwidth/cwidth)
+      $(a).css({
+        'transform':'scale('+ ratio +')',
+        'margin-bottom':- minus,
+        'transform-origin': '0 0'
+      })
+    })
+  }
+  resize()
+  $(window).resize(function(){
+    resize()
+  })
+})()
