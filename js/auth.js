d97127be0d00b0e50a28ac7d2bbd2d3e7b725880
李贤斌 <xianbin.me@qq.com>
2024-10-21 20:39:32 +0800

    Site updated: 2024-10-21 20:38:45

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+(() => {
+  const auth = () => {
+
+    const day = 60 * 60 * 24 * 1000;
+    const { is_post, lock, passwords, root } = window.AD_CONFIG;
+    
+    if(is_post === false || lock === false) {
+      return;
+    }
+    
+    let [password, expires] = atob(window.localStorage.getItem('auth')).split(':'),
+      now = new Date().getTime();
+
+    if(passwords.includes(password) && now < expires) {
+      return; 
+    }
+    password = prompt('验证您是否有权限阅读此文章');
+    password = sha256(password || '');
+    if(passwords.includes(password)) {
+      expires = now + day * 3;
+      window.localStorage.setItem('auth', btoa(`${password}:${expires}`));
+    } else {
+      alert('您没有阅读权限');
+      window.location.href = root;
+    }
+  };
+
+  // article password auth
+  auth();
+})();
